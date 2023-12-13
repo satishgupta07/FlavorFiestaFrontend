bd7175d73476dc764a008da76cf39fe6ec8ee4db
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-12-13 19:35:51 +0530

    Make redux state persist
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,38 @@
-import {configureStore} from '@reduxjs/toolkit';
-import authSlice from './authSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("reduxState", serializedState);
+  } catch (err) {
+    console.error("Error saving state to local storage:", err);
+  }
+};
+
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("reduxState");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    console.error("Error loading state from local storage:", err);
+    return undefined;
+  }
+};
 
 const store = configureStore({
-    reducer: {
-        auth : authSlice,
-        //TODO: add more slices here for posts
-    }
+  reducer: {
+    auth: authSlice,
+    // Add more slices as needed
+  },
+  preloadedState: loadState(),
 });
 
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 export default store;
